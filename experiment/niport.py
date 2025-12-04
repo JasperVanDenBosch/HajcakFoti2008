261021cdcfda4d65c380d2957020532a809af591
Jasper van den Bosch <japsai@gmail.com>
2025-12-04 14:20:30 +0000

    finished ni port

--- experiment/niport.py
+++ experiment/niport.py
@@ -3,14 +3,6 @@ import nidaqmx
 from psychopy.core import wait
 
 
-## DIGITAL PORTS 1-8!!
-
-#port = USB651
-
-
-
-
-
 class NidaqPort:
 
     def __init__(self, address: str):
@@ -20,21 +12,13 @@ class NidaqPort:
         device = nidaqmx.libnidaqmx.Device(address)
         device.reset()
 
-        digitalOutputTask = nidaqmx.DigitalOutputTask(name='MyDOTask')
-        digitalOutputTask.create_channel(DOPORT, name='line0')
-        digitalOutputTask.start()
-
+        ## or DIGITAL PORTS 1-8!!
+        #port = USB651
+        self.do_task = nidaqmx.DigitalOutputTask(name='MyDOTask')
+        self.do_task.create_channel(DOPORT, name='line0')
+        self.do_task.start()
 
     def trigger(self, val: int) -> None:
-
-        for i in range(10):
-            # Set DO port to HIGH
-            digitalOutputTask.write(1)
-            wait(1, hogCPUperiod=1)
-            # Set DO port to LOW
-            digitalOutputTask.write(0)
-            wait(1, hogCPUperiod=1)
-
-# Stop DO signal generation
-digitalOutputTask.stop()
-
+        self.do_task.write(val)
+        wait(0.005, hogCPUperiod=0.005)
+        self.do_task.write(0)
